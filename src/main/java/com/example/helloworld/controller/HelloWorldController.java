37fb7f1d49d502b005ebe7ed08ce151873cf667f
danielmenezesbr <danielmenezes@gmail.com>
2023-11-04 16:08:16 -0300

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 public class HelloWorldController {
